aa5c54a535b7728d601633c25140cdf1c2b2a731
olivebishop <olivehendrilgen1@gmail.com>
2025-02-05 13:40:41 +0300

    fixes

--- components/CookieConsentBanner.tsx
+++ components/CookieConsentBanner.tsx
@@ -30,7 +30,7 @@ const CookieConsentBanner = () => {
 
   return (
     showConsent && (
-      <div className="fixed bottom-0 left-0 right-0 z-50  shadow-lg border-t border-gray-800">
+      <div className="fixed bottom-0 left-0 right-0 z-50 bg-white shadow-lg border-t border-gray-800">
         <div className="w-full max-w-7xl mx-auto px-4 py-4 sm:px-6 sm:py-6">
           <div className="flex flex-col gap-4">
             <div className="space-y-2">
